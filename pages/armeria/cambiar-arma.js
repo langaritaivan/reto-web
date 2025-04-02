5fdf76615585d9d1718ad3f041df10df969067ae
langaritaivan <langaritalatorre24@iespabloserrano.com>
2025-04-02 09:35:24 +0200

    todas armas

--- pages/armeria/cambiar-arma.js
+++ pages/armeria/cambiar-arma.js
@@ -3,19 +3,19 @@ const armeria = document.getElementById("armeria")
 selectArmas.addEventListener("change",()=>{
   switch (selectArmas.value) {
     case "arma1":
-      armeria.style.backgroundImage="url('../../assets/img/SVA.jpg')"
+      armeria.style.backgroundImage="url('../../assets/img/kilo.jpg')"
       break;
     case "arma2":
-      armeria.style.backgroundImage = "url('../../assets/img/ak47.jpg)"
+      armeria.style.backgroundImage = "url('../../assets/img/ak47.jpg')"
       break;
     case "arma3":
-      armeria.style.backgroundImage = "url('../../assets/img)"
+      armeria.style.backgroundImage = "url('../../assets/img/riveter.jpg')"
       break;
     case "arma4":
-      armeria.style.backgroundImage = "url('../../assets/img)"
+      armeria.style.backgroundImage = "url('../../assets/img/MP5.jpg')"
       break;
     case "arma5":
-      armeria.style.backgroundImage = "url('../../assets/img)"
+      armeria.style.backgroundImage = "url('../../assets/img/SVA.jpg')"
       break;
     
 
